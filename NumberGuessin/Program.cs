5087ed5129c3d7b67f85214d0cda8b22f5272da1
STUDENTMicahSands <STUDENTMicahSands@TECH07>
2023-10-27 08:56:19 -0700

    Add project files.

---
+++ NumberGuessin/Program.cs
@@ -0,0 +1,30 @@
+﻿static void Main(string[] args)
+{
+    Random random = new Random();
+
+    int returnValue = random.Next(1,100);
+    int Guess = 0;
+
+    Console.WriteLine("Guess a number between 1 & 100");
+
+    while (Guess != returnValue)
+    {
+        Guess = Convert.ToInt32(Console.ReadLine());
+
+        while (Guess < returnValue)
+        {
+            Console.WriteLine("Number is Higher");
+            Console.ReadLine();
+        }
+        while (Guess > returnValue)
+        {
+            Console.WriteLine("Number is Lower");
+            Console.ReadLine();
+        }
+    }
+    while (Guess == returnValue)
+    {
+        Console.WriteLine($"Congrats the answer was {returnValue}");
+        Console.ReadLine();
+    }
+}
